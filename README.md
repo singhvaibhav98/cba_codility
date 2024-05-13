56cd9f7305650427ef7a3c57e8e6a04577369013
singhvaibhav98 <vaibhavei2051@gmail.com>
2024-05-13 19:35:01 +1000

    First Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# Cypress Automation Excercise
+
+UI & API Automation Assignment
+
+#### Key Features
+- Cucumber based cypress framework implementation
+- API Automation using cypress
+- Mochawesome reporting
+- Github action configured
+
+![](image.png)
+![](run.png)
---
+++ cypress.config.js
@@ -0,0 +1,37 @@
+const {defineConfig} = require("cypress");
+const cucumber = require('cypress-cucumber-preprocessor').default;
+
+module.exports = defineConfig({
+    projectId: 'j7zetb',
+    experimentalCspAllowList: true,
+    experimentalWebKitSupport: true,
+    includeShadowDom: true,
+    chromeWebSecurity: false,
+    experimentalSourceRewriting: true,
+    screenshotsFolder: 'reports/mochawesome/screenshots',
+    screenshotOnRunFailure: true,
+    viewportWidth: 1536,
+    viewportHeight: 960,
+    e2e: {
+        defaultCommandTimeout: 1000,
+        experimentalRunAllSpecs: true,
+        setupNodeEvents(on, config) {
+            on('file:preprocessor', cucumber());
+            require('cypress-mochawesome-reporter/plugin')(on);
+            return config;
+        },
+        specPattern: 'cypress/(e2e|api)/*.*',
+        supportFile: false
+    },
+    reporter: 'cypress-mochawesome-reporter',
+    video: true,
+    reporterOptions: {
+        reportDir: 'reports/mochawesome',
+        charts: true,
+        reportPageTitle: 'Test Automation Result',
+        embeddedScreenshots: true,
+        inlineAssets: false,
+        saveAllAttempts: true,
+        debug: true,
+    }
+});
